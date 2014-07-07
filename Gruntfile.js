a6b17471577ec4f1ad4a7c01a2b40889ce33fcd9
Brandon Copeland <br@ndoncopeland.com>
2014-07-07 10:57:07 -0500

    adds demo

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,7 +6,16 @@ module.exports = function (grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     clean: {
-      dist: 'dist'
+      dist: 'dist',
+      demo: 'dist/demo'
+    },
+    copy: {
+      demo: {
+        files: [{
+          src: 'demo/*.html',
+          dest: 'dist/'
+        }]
+      }
     },
     sass: {
       dist: {
@@ -16,11 +25,17 @@ module.exports = function (grunt) {
         files: {
           'dist/css/<%= pkg.name %>.min.css': 'scss/<%= pkg.name %>.scss'
         }
+      },
+      demo: {
+        files: {
+          'dist/demo/main.css': 'demo/main.scss'
+        }
       }
     }
   });
 
-  grunt.registerTask('dist', ['clean', 'sass:dist']);
+  grunt.registerTask('demo', ['clean:demo', 'copy:demo', 'sass:demo']);
+  grunt.registerTask('dist', ['clean:dist', 'sass:dist']);
 
   grunt.registerTask('default', 'dist');
 };
